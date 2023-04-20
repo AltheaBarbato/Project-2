9967d7a2971591fa97ceece7360fd01d549bf7ec
AltheaBarbato <122847641+AltheaBarbato@users.noreply.github.com>
2023-04-19 19:51:21 -0500

    trying to fix map

--- js/script.js
+++ js/script.js
@@ -1,43 +1,13 @@
-function myMap() {
-  var mapProp = {
-    center: new google.maps.LatLng(41.87552, -87.630717),
-    zoom: 10,
-  };
-  var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
-  myPointer(map);
+let map;
 
-  google.maps.event.addListener(map, "click", function(event){
-    movePtr(map, event.latLng);
+async function initMap() {
+  //@ts-ignore
+  const { Map } = await google.maps.importLibrary("maps");
+
+  map = new Map(document.getElementById("map"), {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 8,
   });
 }
 
-function movePtr(map, location) {
-    var ptr = new google.maps.Marker({
-      position: location,
-      map: map
-    });
-    var infowindow = new google.maps.InfoWindow({
-      content: 'Latitude: ' + location.lat() +
-      '<br>Longitude: ' + location.lng()
-    });
-    infowindow.open(map, ptr);
-
-    setTimeout(function() {
-        ptr.setMap(null);
-      }, 5500);
-  }
-
-function myPointer(map) {
-  var infowindow = new google.maps.InfoWindow({
-    content: "This is Chicago!",
-  });
-  var ptr = new google.maps.Marker({
-    position: new google.maps.LatLng(41.87552, -87.630717),
-  });
-  ptr.setMap(map);
-  google.maps.event.addListener(ptr, "click", function () {
-    infowindow.open(map, ptr);
-    map.setZoom(12);
-    map.setCenter(ptr.getPosition());
-  });
-}
+initMap();
