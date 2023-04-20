a73dcd7509daea945f75b0e9b72dd9185b3a718a
AltheaBarbato <122847641+AltheaBarbato@users.noreply.github.com>
2023-04-19 22:11:00 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -12,3 +12,22 @@ async function initMap() {
 
 initMap();
 
+var empireStateBuilding = new google.maps.Marker({
+      position: {lat: 35.10295383110616, lng: 33.244096104232426},
+      map: map,
+      title: 'Where my family is from '
+    });
+  
+    // Add an info window to the Empire State Building marker
+    var empireStateInfoWindow = new google.maps.InfoWindow({
+      content: '<h2>Empire State Building</h2><p>A 102-story skyscraper located in Midtown Manhattan.</p>'
+    });
+    empireStateInfoWindow.open(map, empireStateBuilding);
+  
+    // Add a button to center the map on the Statue of Liberty
+    var centerButton = document.getElementById('center-btn');
+    centerButton.addEventListener('click', function() {
+      map.setCenter({lat: 35.10295383110616, lng: 33.244096104232426});
+      map.setZoom(15);
+    });
+  }
