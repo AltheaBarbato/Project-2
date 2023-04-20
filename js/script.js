3f0c2de56dc01083ad7bf01f6761c68eced80e3e
AltheaBarbato <122847641+AltheaBarbato@users.noreply.github.com>
2023-04-19 22:19:03 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 function initMap() {
 
 
-  var map = new Map(document.getElementById("map"), {
+  var map = new google.maps.Map(document.getElementById("map"), {
     center: { lat: 35.10295383110616, lng: 33.244096104232426 },
     zoom: 8,
   });
