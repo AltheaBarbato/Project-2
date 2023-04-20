7b73532aad81cb617ef40191f84f9cdab549338c
AltheaBarbato <122847641+AltheaBarbato@users.noreply.github.com>
2023-04-19 22:01:46 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -14,16 +14,14 @@ initMap();
 
 var myMap = new google.maps.Map(el, mapOptions);
 
-var myMap = new google.maps.Map(el, mapOptions);
-
 	var marker = new google.maps.Marker({
 		position: myLocation,
 		map: myMap,
 		animation: google.maps.Animation.BOUNCE,
-		icon: 'pics/icon.jpg'
+		icon: 'pics/icon.png'
 	});
-	
-var contentString = '<h1>IIT Perlstein Hall</h1><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate odit optio, voluptatem placeat odio dignissimos illo magnam esse asperiores voluptas at iure vero eum, nemo aperiam? Ipsam, atque nobis rem.</p>';
+
+	var contentString = '<h1>IIT Perlstein Hall</h1><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate odit optio, voluptatem placeat odio dignissimos illo magnam esse asperiores voluptas at iure vero eum, nemo aperiam? Ipsam, atque nobis rem.</p>';
 
 	var infowindow = new google.maps.InfoWindow({
       content: contentString
@@ -32,3 +30,6 @@ var contentString = '<h1>IIT Perlstein Hall</h1><p>Lorem ipsum dolor sit amet, c
 	google.maps.event.addListener(marker, 'mouseover', function() {
     	infowindow.open(myMap, marker);
   	});
+
+
+}
