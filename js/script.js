9e1de493bc4064fe7a5a89d8e790200bbb5c2378
AltheaBarbato <122847641+AltheaBarbato@users.noreply.github.com>
2023-04-20 22:17:02 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -13,7 +13,7 @@ async function initMap() {
 	var myMap = new google.maps.Map(el, mapOptions);
 
 	var marker = new google.maps.Marker({
-		position: myLocation,
+		position: { lat: 35.10295383110616, lng: 33.244096104232426 },
 		map: myMap,
 		animation: google.maps.Animation.BOUNCE,
 		icon: 'pics/icon.png'
