8929adf716834f19f11e3cb479bd6a52eb07270c
AltheaBarbato <122847641+AltheaBarbato@users.noreply.github.com>
2023-04-20 22:29:47 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ async function initMap() {
   //@ts-ignore
   const { Map } = await google.maps.importLibrary("maps");
 
-  var map = new Map(document.getElementById("map"), {
+  map = new Map(document.getElementById("map"), {
     center: { lat: 35.10295383110616, lng: 33.244096104232426 },
     zoom: 8,
   });
