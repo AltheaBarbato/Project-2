abe495966607c953d7fd5415d29f0cb38fe9c343
AltheaBarbato <122847641+AltheaBarbato@users.noreply.github.com>
2023-04-26 17:20:43 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 
   let map;
 
-async function initMap() {
+/*async function initMap() {
   //@ts-ignore
   const { Map } = await google.maps.importLibrary("maps");
 
@@ -34,3 +34,20 @@ initMap();
 }
 
 google.maps.event.addDomListener(window, 'load', init);
+*/
+
+let myLocation = {lat: 35.10295383110616, lng: 33.244096104232426}
+
+async funciton initMap() {
+	//@ts-ignore
+	map = new google.maps.Map(document.getElementById("map), {
+		center: garden,
+		zoom: 8,
+	});
+
+	let marker = new google.maps.Marker({
+		position: myMarker,
+		map: map,
+		title: "This is where my family is from"
+	});
+}	
